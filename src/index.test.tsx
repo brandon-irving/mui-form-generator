2e45518d9e62e14c29ff87067c654e5bbdc0507b
birving2 <brandon@peakactivity.com>
2020-12-10 23:58:38 -0500

    added lint file

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,7 +1,7 @@
-import { ExampleComponent } from './index'
+import MuiFormGenerator from './MuiFormGenerator'
 
-describe('ExampleComponent', () => {
+describe('MuiFormGenerator', () => {
   it('is truthy', () => {
-    expect(ExampleComponent).toBeTruthy()
+    expect(MuiFormGenerator).toBeTruthy()
   })
 })
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,1 @@
-import * as React from 'react'
-
-
-export const ExampleComponent = () => {
-  return <div>Example Component</div>
-}
-
 export {default as MuiFormGenerator } from './MuiFormGenerator'
